02443791bfc75790cac56cf724bb2886ede63e6a
Nabeel Goraya <itsgoraya@gmail.com>
2023-12-13 19:08:36 +0500

    Update quota.py

--- erpnext_quota/erpnext_quota/quota.py
+++ erpnext_quota/erpnext_quota/quota.py
@@ -1,19 +1,22 @@
-import subprocess
-
 import frappe
-from frappe import _
 import json
 
 def document_limit(doc, event):
-    doctype_name = doc.doctype
-    limit = frappe.get_site_config()['quota']
-    doc_list = json.loads(limit) 
-    
-    for item in doc_list:
-        if doctype_name in item:
-            doc_count = item[doctype_name]
-            count = frappe.db.count(doctype_name)
-            if count > doc_count:
-                frappe.throw(f"You have exceeded the maximum limit of {doctype_name}. Limit: {doc_count}, Current Count: {count}.")
-            else:
-                pass
+    try:
+        doctype_name = doc.doctype
+        limit_config = frappe.get_site_config().get('quota')
+
+        if limit_config:
+            doc_list = json.loads(limit_config)
+
+            for item in doc_list:
+                if doctype_name in item:
+                    doc_count = item[doctype_name]
+                    count = frappe.db.count(doctype_name)
+                    if count > doc_count:
+                        frappe.throw(f"You have exceeded the maximum limit of {doctype_name}. Limit: {doc_count}, Current Count: {count}.")
+                    else:
+                        pass
+
+    except KeyError:
+        pass
